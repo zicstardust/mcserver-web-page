8341ad974847dc9397a6a7883c1e64cdbf1d9a1f
Zic <git@zicstardust.com>
2024-12-29 12:26:07 -0400

    fix server_status

--- lib/server_status.py
+++ lib/server_status.py
@@ -1,23 +1,16 @@
 import json
 import requests
-from os import environ
+from .load_env import crafty_url, crafty_api_key, crafty_server_id
+from .check_api import check_api
 
-crafty_url=environ.get("CRAFTY_URL","")
-crafty_api_key=environ.get("CRAFTY_API_KEY","")
-crafty_server_id=environ.get("CRAFTY_SERVER_ID","")
-
-api_url = "{}/api/v2/servers/{}/stats".format(crafty_url,crafty_server_id)
-head = {'Authorization': 'Bearer {}'.format(crafty_api_key)}
-
+api_status = check_api(crafty_url, crafty_api_key, crafty_server_id)
 
 def get_server_status():
-    try:
+    if api_status == "Ok":
+        api_url = "{}/api/v2/servers/{}/stats".format(crafty_url,crafty_server_id)
+        head = {'Authorization': 'Bearer {}'.format(crafty_api_key)}
         response = requests.get(api_url, headers=head)
-    except requests.exceptions.ConnectionError:
-        return "Connection Error"
-
-    if response.status_code == 200:
         running = (json.dumps(response.json()['data']['running'], indent=4))
         players_online = (json.dumps(response.json()['data']['online'], indent=4))
         return dict(running=running, players_online=players_online)
-    return "Response error"
+    return api_status
